58e8d0596861c706f2bd8645d2737d660d52fad8
David BARBOUTIE <d.barboutie@cite-scolaire.lan>
2024-02-16 11:07:15 +0100

    orrection de bug: bug de merge

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -22,25 +22,37 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+
   const MyHomePage({super.key, required this.title});
-
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  }
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+      appBar: AppBar(                                                     //appbar partie de noe
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,        
       ),
+
+        body:Center(
+        child: Column(
+          //ListView storie, partie de JC 
+          children: [ListView(
+
+          ),
+          //ListView publication, partie de david
+          Expanded(child: ListView(
+              
+          ))],
+        ),
+        )
+        
     );
   }
 }
