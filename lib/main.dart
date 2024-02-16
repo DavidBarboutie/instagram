bc34467442e52281feb5d7a728799696b9b6115e
btssio <dnoe.pro@gmail.com>
2024-02-16 12:28:57 +0100

    modification : AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -23,24 +23,41 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Pour vous"),
+          backgroundColor: Colors.white,
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.heart_broken),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.arrow_circle_right_rounded),
+              onPressed: () {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(const SnackBar(content: Text('Next Page')));
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          child: Column(
+            //ListView storie, partie de JC
+            children: [
+              ListView(),
+              //ListView publication, partie de david
+              Expanded(child: ListView())
+            ],
+          ),
+        ));
   }
 }
