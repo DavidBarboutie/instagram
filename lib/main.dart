47e418f8f81581c2c3cea9fbdd2e68ba93b95727
David BARBOUTIE <d.barboutie@cite-scolaire.lan>
2024-02-16 10:48:57 +0100

    correction de bug: bug de changement de branche

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,19 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+<<<<<<< HEAD
   // This widget is the root of your application.
+=======
+>>>>>>> main
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+<<<<<<< HEAD
+=======
+
+>>>>>>> main
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -22,6 +29,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+<<<<<<< HEAD
 
   const MyHomePage({super.key, required this.title});
   final String title;
@@ -32,10 +40,24 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   
+=======
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  }
+>>>>>>> main
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+<<<<<<< HEAD
       appBar: AppBar(                                                     //appbar partie de noe
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,        
       ),
@@ -56,3 +78,12 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+=======
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+    );
+  }
+}
+>>>>>>> main
