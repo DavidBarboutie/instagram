f67340d29e857638c62128294eed26ce65aee467
Jean-Claude LAFARGUE <j.lafargue@cite-scolaire.lan>
2024-02-16 11:31:07 +0100

    correction de bug: rectification des erreurs du à l'indentation

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -24,7 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -32,8 +30,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,6 +37,15 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
+      body: Column(children: <Widget>[
+        const Text(
+          'You have pushed the button this many times:',
+        ),
+        Text(
+          'JSP',
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ]),
     );
   }
 }
