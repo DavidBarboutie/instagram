c734ef276fce3490ad756a1db1a11c3e941b0d02
David BARBOUTIE <d.barboutie@cite-scolaire.lan>
2024-02-16 12:49:42 +0100

    modification: completion plus pousser du squelette des publications; ajout de tailles

--- lib/main.dart
+++ lib/main.dart
@@ -45,35 +45,42 @@ class _MyHomePageState extends State<MyHomePage> {
         body:Center(
         child: Column(
           //ListView storie, partie de JC 
-          children: [ListView(
-
+          children: [Container(
+            height: 100,
+            color: Colors.blue,
+            child: ListView(
+            
+            ),
           ),
+          
           //ListView publication, partie de david
-          ListView(
-          
-              //colonne qui correspond a une unique publication
-              children: [Column(
-          
-                //ligne qui comporte la photo de profil et le nom d'utilisateur au dessus de la publication
-                children: [Row(
-                  //photo de profil
-                    children: [Image.asset(""),
-                    //nom d'utilisateur
-                    Text("")], 
-                ),
-                Text(""), //carrousel a faire
-                //bas de publication
-                Column(
-                  //like, commentaire, partager
+          Container(
+            height: 603,
+            color: Colors.black,
+            child: ListView(
+            
+                //colonne qui correspond a une unique publication
+                children: [Column(
+            
+                  //ligne qui comporte la photo de profil et le nom d'utilisateur au dessus de la publication
                   children: [Row(
-                    children: [
-                    Image.asset(""),
-                    Image.asset(""),
-                    Image.asset("")],
+                    //photo de profil
+                      children: [Text("photo de profil"),
+                      //nom d'utilisateur
+                      Text("username")], 
                   ),
-                  Text("nb_like"+"j'aime"),Text("user_id"+"description"),Text("il y a"+"date_post"),], 
+                  Text(""), //carrousel a faire
+                  //bas de publication
+                  Column(
+                    //like, commentaire, partager
+                    children: [Row(
+                      children: [
+                      Text("LIKE"),Text("COMMENT"),Text("SHARE")],
+                    ),
+                    Text("nb_like"+"j'aime"),Text("user_id"+"description"),Text("il y a"+"date_post"),], 
+                  )],
                 )],
-              )],
+            ),
           )],
         ),
         )
