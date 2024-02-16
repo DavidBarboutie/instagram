cc1f3413fc495c642a452ca49ca6669237a3578c
David BARBOUTIE <d.barboutie@cite-scolaire.lan>
2024-02-16 12:20:39 +0100

    ajout: deserealisation du json

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
 
 void main() {
   runApp(const MyApp());
@@ -6,7 +8,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -27,7 +29,6 @@ class MyHomePage extends StatefulWidget {
   final String title;
   @override
   State<MyHomePage> createState() => _MyHomePageState();
-
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -35,6 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       appBar: AppBar(                                                     //appbar partie de noe
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,        
@@ -71,12 +73,41 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   Text("nb_like"+"j'aime"),Text("user_id"+"description"),Text("il y a"+"date_post"),], 
                 )],
-                
               )],
           )],
         ),
         )
-        
     );
   }
+}
+
+class Person {
+  int userId;
+  List<String> image;
+  int nb_like;
+  String description;
+  String date_post;
+
+  Person({required this.userId, required this.image, required this.nb_like, required this.description, required this.date_post});
+
+  factory Person.fromJson(Map<String, dynamic> json) {
+    return Person(
+      userId: json['userId'],
+      image: json['images'],
+      nb_like: json['nb_like'],
+      description: json['description'],
+      date_post: json['date_post']
+    );
+  }
+}
+
+Future<Person> loadPersonFromJson() async {
+  // Charger le contenu du fichier JSON
+  final String response = await rootBundle.loadString('assets/data.json');
+  
+  // Convertir la chaîne JSON en un objet Map
+  final data = jsonDecode(response);
+
+  // Créer une instance de Person à partir du JSON
+  return Person.fromJson(data);
 }
