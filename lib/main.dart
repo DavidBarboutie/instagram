a09de8d0ad8df59ab5a1d77563bc2d9623b890ad
btssio <btssio@PC-00330-EJA.cite-scolaire.lan>
2024-03-08 11:46:15 +0100

    ajout fonctionnalité menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+//utilisation pour le mneu 
+enum SampleItem { itemOne, itemTwo, itemThree }
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,27 +23,46 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
+  String selectedOption = 'None';
+
+void selectOption(String option) {
+    setState(() {
+      selectedOption = option;
+    });
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Pour vous"),
+        appBar: AppBar( 
+          title: const Text('Pour vous'),
           backgroundColor: Colors.white,
           actions: <Widget>[
+            PopupMenuButton<String>(
+            onSelected: selectOption,
+            itemBuilder: (BuildContext context) {
+              return {'Option 1', 'Option 2'}.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            },
+          ),
             IconButton(
               icon: const Icon(Icons.heart_broken),
               onPressed: () {},
             ),
             IconButton(
-              icon: const Icon(Icons.arrow_circle_right_rounded),
+              icon: const Icon(Icons.send),
               onPressed: () {
                 ScaffoldMessenger.of(context)
                     .showSnackBar(const SnackBar(content: Text('Next Page')));
@@ -51,13 +72,14 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: Center(
           child: Column(
-            //ListView storie, partie de JC
+            //partie de JC
             children: [
               ListView(),
-              //ListView publication, partie de david
+              //partie de david
               Expanded(child: ListView())
             ],
           ),
-        ));
+        )
+    );
   }
 }
